86ab160185543ca1221cc94b0bb09501bed94c1e
Szymon Pobiega <szymon@pobiega.com>
2011-03-04 12:45:01 +0100

    Metadata

--- src/Restbucks.Service/Global.asax.cs
+++ src/Restbucks.Service/Global.asax.cs
@@ -1,25 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
 using System.Web;
-using System.Web.Routing;
-using System.Web.Security;
-using System.Web.SessionState;
 using Autofac;
 
 namespace Restbucks.Service
 {
-    public class Global : System.Web.HttpApplication
+    public class Global : HttpApplication
     {
+        private static RestbucksConfiguration _configuration;
+
+        public static RestbucksConfiguration Configuration
+        {
+            get { return _configuration; }
+        }
 
         protected void Application_Start(object sender, EventArgs e)
         {
             var containerBuilder = new ContainerBuilder();
             containerBuilder.RegisterModule(new MainModule());
             var container = containerBuilder.Build();
-            var configuration = new RestbucksConfiguration(container);
-            RestbucksResources.RegisterRoutes(configuration);
+            _configuration = new RestbucksConfiguration(container);
+            //RestbucksResources.RegisterRoutes(Configuration);
         }
     }
 }
---
+++ src/Restbucks.Service/RestbucksHostFactory.cs
@@ -0,0 +1,14 @@
+using System;
+using System.ServiceModel.Activation;
+using Microsoft.ServiceModel.Http;
+
+namespace Restbucks.Service
+{
+    public class RestbucksHostFactory : ServiceHostFactory
+    {
+        protected override System.ServiceModel.ServiceHost CreateServiceHost(Type serviceType, Uri[] baseAddresses)
+        {
+            return new WebHttpServiceHost(serviceType, Global.Configuration, baseAddresses);
+        }
+    }
+}
