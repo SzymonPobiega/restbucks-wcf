75e92d81d48961c07b629e298dc0819365a935a4
Szymon Pobiega <szymon@pobiega.com>
2011-03-11 15:40:05 +0100

    Corrected caseing in Size enum

--- src/Restbucks.Service/Domain/Size.cs
+++ src/Restbucks.Service/Domain/Size.cs
@@ -1,9 +1,14 @@
-﻿namespace Restbucks.Service.Domain
+﻿using System.Xml.Serialization;
+
+namespace Restbucks.Service.Domain
 {
     public enum Size
     {
+        [XmlEnum("small")]
         Small,
+        [XmlEnum("medium")]
         Medium,
+        [XmlEnum("large")]
         Large
     }
 }
